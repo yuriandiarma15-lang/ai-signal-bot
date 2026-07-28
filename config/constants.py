cf40ec0f2dc79a9243d96aa1840419c030602935
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-07-28 22:04:39 +0700

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,47 @@
+from datetime import time
+
+
+
+# ==========================
+# TRADING SESSION
+# ==========================
+
+
+START_DAY = 0
+# Monday
+
+
+END_DAY = 5
+# Saturday
+
+
+
+START_TIME = time(
+    7,
+    0
+)
+
+
+END_TIME = time(
+    2,
+    0
+)
+
+
+
+# ==========================
+# SIGNAL
+# ==========================
+
+
+MAX_SIGNAL_PER_DAY = 20
+
+
+
+SIGNAL_PREFIX = """
+
+━━━━━━━━━━━━━━
+
+🤖 <b>XAU AI ASSISTANT</b>
+
+"""
