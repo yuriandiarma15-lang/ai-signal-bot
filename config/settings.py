9559cc9dd1d512cdf877fcab1266736e78f33505
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-07-28 22:21:24 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,47 +2,30 @@ import os
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 
-
-BOT_TOKEN = os.getenv(
-    "BOT_TOKEN"
-)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
-
-TWELVE_TOKEN = os.getenv(
-    "TWELVE_TOKEN"
-)
-
+TWELVE_TOKEN = os.getenv("TWELVE_TOKEN")
 
 
 SOURCE_GROUP_ID = int(
     os.getenv(
-        "SOURCE_GROUP_ID"
+        "SOURCE_GROUP_ID",
+        "0"
     )
 )
 
 
-
-SPREADSHEET_ID = os.getenv(
-    "SPREADSHEET_ID"
-)
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 
 
-
-ADMIN_USERNAME = os.getenv(
-    "ADMIN_USERNAME"
-)
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 
 
-
-RENEW_BOT = os.getenv(
-    "RENEW_BOT"
-)
-
+RENEW_BOT = os.getenv("RENEW_BOT")
 
 
 TIMEZONE = os.getenv(
