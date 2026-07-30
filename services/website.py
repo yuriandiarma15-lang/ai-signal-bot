141d1629e8442be836519533b6e6a22e12d7c474
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-07-30 07:02:31 +0700

    Update website.py

--- services/website.py
+++ services/website.py
@@ -1,15 +1,21 @@
 import aiohttp
+
 from config.settings import WEBSITE_URL, API_KEY
 
 
 async def send_signal_to_website(signal):
+
     headers = {
-        "Authorization": f"Bearer {API_KEY}"
+        "x-api-key": API_KEY,
+        "Content-Type": "application/json"
     }
 
     async with aiohttp.ClientSession() as session:
-        await session.post(
+
+        async with session.post(
             WEBSITE_URL,
             json=signal,
             headers=headers
-        )
+        ) as response:
+
+            print(await response.text())
